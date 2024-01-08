c3daf9b1f441516dd76255ceb3973f5131d75f42
Ember <sanshi1929@gmail.com>
2024-01-08 09:15:22 +0900

    fix devs

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'prettier'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
@@ -17,6 +17,38 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'max-len': [
+      'error',
+      {
+        ignoreUrls: true,
+        ignoreStrings: true,
+        code: 120,
+      },
+    ],
+    'node/no-unpublished-require': 0,
+    eqeqeq: 2,
+    'no-return-assign': 2,
+    'no-return-await': 2,
+    'no-console': [
+      'error',
+      {
+        allow: ['info', 'warn', 'error'],
+      },
+    ],
+    'line-comment-position': [
+      'error',
+      {
+        position: 'above',
+      },
+    ],
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
